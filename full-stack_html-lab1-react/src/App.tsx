2ecee4dd59cad3ff65f226599fa1d51519b9febf
Marion Queen Ramos <mramos3@academic.rrc.ca>
2025-09-28 12:15:38 -0500

    debug: fix routing - App was missing routes setup for organization page.

--- full-stack_html-lab1-react/src/App.tsx
+++ full-stack_html-lab1-react/src/App.tsx
@@ -1,15 +1,22 @@
+import { Route, Routes } from "react-router";
 import Nav from './components/nav';
 import Header from './components/header';
-import EmployeeDirectory from './components/employee_directory';
 import Footer from './components/footer';
-
+import EmployeeDirectory from './components/employee_directory';
+import Organization from './components/organization';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
-      <EmployeeDirectory />
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<EmployeeDirectory />} />
+          <Route path="/employees" element={<EmployeeDirectory />} />
+          <Route path="/organization" element={<Organization />} />
+        </Routes>
+      </main>
       <Footer />
     </div>
   );
