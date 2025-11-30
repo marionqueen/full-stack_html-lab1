bc6e2f922b904ef04da74976dedffd2a99ac1c96
Marion Queen Ramos <mramos3@academic.rrc.ca>
2025-11-29 20:16:25 -0600

    feat: Add ClerkProvider to main.tsx.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,20 @@ import './App.css';
 import App from './App.tsx';
 import { BrowserRouter } from "react-router";
 import { StrictMode } from "react";
+import { ClerkProvider } from '@clerk/clerk-react';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk Publishable Key");
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>
 );
